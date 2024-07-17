74f05ecda00e23787edccd07c5209dfd964fc60c
cmn <cmn@discente.ifpe.edu.br>
2024-07-17 02:22:12 -0300

    add about project

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -1,16 +1,3 @@
-// Função da tela preta do inicio
-setTimeout(function () {
-  var telaPreta = document.getElementById("black-screen");
-  telaPreta.style.opacity = 0;
-  setTimeout(function () {
-    telaPreta.style.display = "none";
-  }, 1500);
-}, 5000); // Definir os segundos de duração dessa tela
-// Funcionalidade pra voltar para o inicio da pagina
-function voltarAoTopo() {
-  document.documentElement.scrollTop = 0;
-}
-
 function exibirBotao() {
   var botao = document.getElementById("voltarTopo");
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -30,3 +17,43 @@ function scrollToSection() {
     ideaSection.scrollIntoView({ behavior: "smooth" });
   }
 }
+
+// Função para trocar o conceito exibido
+function trocarConceito(conceito) {
+  // Seleciona os containers das divs
+  var ecoContainer = document.getElementById("desc-eco");
+  var somBinauralContainer = document.getElementById("desc-binaural");
+  var sinesContainer = document.getElementById("desc-sinestesia");
+
+  // Verifica qual conceito deve ser exibido e oculta os outros
+  if (conceito === "desc-eco") {
+    ecoContainer.classList.add("active");
+    somBinauralContainer.classList.remove("active");
+    sinesContainer.classList.remove("active");
+  } else if (conceito === "desc-binaural") {
+    ecoContainer.classList.remove("active");
+    somBinauralContainer.classList.add("active");
+    sinesContainer.classList.remove("active");
+  } else if (conceito === "desc-sinestesia") {
+    ecoContainer.classList.remove("active");
+    somBinauralContainer.classList.remove("active");
+    sinesContainer.classList.add("active");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Inicia mostrando apenas o conteúdo de ecolocalização
+  var ecoContainer = document.getElementById("desc-eco");
+  ecoContainer.classList.add("active");
+
+  // Adiciona eventos de clique aos botões para trocar o conceito
+  document.getElementById("btnEco").addEventListener("click", function () {
+    trocarConceito("desc-eco");
+  });
+  document.getElementById("btnSomB").addEventListener("click", function () {
+    trocarConceito("desc-binaural");
+  });
+  document.getElementById("btnSines").addEventListener("click", function () {
+    trocarConceito("desc-sinestesia");
+  });
+});
